9c1cbdceaa3b2abb47d8d605f4653e6f61d15dd7
Artem Eremchuk <eremchuk.it@gmail.com>
2023-05-20 10:36:13 +0300

    Add url location for habbit

--- scripts/app.js
+++ scripts/app.js
@@ -152,6 +152,7 @@ function rerender(activeHabbitId) {
     return;
   }
 
+  document.location.replace(document.location.pathname + `#${activeHabbitId}`);
   rerenderMenu(activeHabbit);
   rerenderHead(activeHabbit);
   rerenderContent(activeHabbit);
@@ -237,5 +238,12 @@ function addHabbit(event) {
 /* init */
 (() => {
   loadData();
-  rerender(habbits[0].id);
+  const hashId = Number(document.location.hash.replace("#", ""));
+  const urlHabbit = habbits.find((habbit) => habbit.id === hashId);
+
+  if (urlHabbit) {
+    rerender(urlHabbit.id);
+  } else {
+    rerender(habbits[0].id);
+  }
 })();
